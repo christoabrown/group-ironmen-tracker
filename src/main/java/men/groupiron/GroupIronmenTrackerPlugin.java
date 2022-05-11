99c4a8aa867ad95f620aff0e4259b029d4911bcd
Chris Brown <christoabrown@gmail.com>
2022-05-11 10:01:42 -0500

    fix group storage not saving in some cases

--- src/main/java/men/groupiron/GroupIronmenTrackerPlugin.java
+++ src/main/java/men/groupiron/GroupIronmenTrackerPlugin.java
@@ -6,6 +6,7 @@ import net.runelite.api.*;
 import net.runelite.api.coords.LocalPoint;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.*;
+import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -33,12 +34,11 @@ public class GroupIronmenTrackerPlugin extends Plugin {
     private int itemsDeposited = 0;
     private static final int SECONDS_BETWEEN_UPLOADS = 1;
     private static final int SECONDS_BETWEEN_INFREQUENT_DATA_CHANGES = 60;
-    private static final int SAVE_SHARED_STORAGE = 47448098;
-    private static final int BACK_TO_BANK_SHARED_STORAGE = 47448073;
     private static final int DEPOSIT_ITEM = 12582914;
     private static final int DEPOSIT_INVENTORY = 12582916;
     private static final int DEPOSIT_EQUIPMENT = 12582918;
     private static final int CHATBOX_ENTERED = 681;
+    private static final int GROUP_STORAGE_LOADER = 293;
 
     @Override
     protected void startUp() throws Exception {
@@ -92,6 +92,13 @@ public class GroupIronmenTrackerPlugin extends Plugin {
     public void onGameTick(GameTick gameTick) {
         --itemsDeposited;
         updateInteracting();
+
+        Widget groupStorageLoaderText = client.getWidget(GROUP_STORAGE_LOADER, 1);
+        if (groupStorageLoaderText != null) {
+            if (groupStorageLoaderText.getText().equalsIgnoreCase("saving...")) {
+                dataManager.getSharedBank().commitTransaction();
+            }
+        }
     }
 
     @Subscribe
@@ -114,7 +121,7 @@ public class GroupIronmenTrackerPlugin extends Plugin {
             dataManager.getDeposited().reset();
             dataManager.getBank().update(new ItemContainerState(playerName, container, itemManager));
         } else if (id == InventoryID.SEED_VAULT.getId()) {
-          dataManager.getSeedVault().update(new ItemContainerState(playerName, container, itemManager));
+            dataManager.getSeedVault().update(new ItemContainerState(playerName, container, itemManager));
         } else if (id == InventoryID.INVENTORY.getId()) {
             ItemContainerState newInventoryState = new ItemContainerState(playerName, container, itemManager, 28);
             if (itemsDeposited > 0) {
@@ -146,9 +153,7 @@ public class GroupIronmenTrackerPlugin extends Plugin {
         final int param1 = event.getParam1();
         final MenuAction menuAction = event.getMenuAction();
         if (menuAction == MenuAction.CC_OP) {
-            if (param1 == SAVE_SHARED_STORAGE || param1 == BACK_TO_BANK_SHARED_STORAGE) {
-                dataManager.getSharedBank().commitTransaction();
-            } else if (param1 == DEPOSIT_ITEM || param1 == DEPOSIT_INVENTORY || param1 == DEPOSIT_EQUIPMENT) {
+            if (param1 == DEPOSIT_ITEM || param1 == DEPOSIT_INVENTORY || param1 == DEPOSIT_EQUIPMENT) {
                 itemsMayHaveBeenDeposited();
             }
         }
