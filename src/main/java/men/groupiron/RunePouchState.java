ada71de1222e805dbedbdb3f843092bb401c5826
Chris Brown <christoabrown@gmail.com>
2022-09-08 17:41:42 -0500

    block pvp arena world, fix rune pouch api change, 4 slot rune pouch

--- src/main/java/men/groupiron/RunePouchState.java
+++ src/main/java/men/groupiron/RunePouchState.java
@@ -1,34 +1,33 @@
 package men.groupiron;
 
 import net.runelite.api.Client;
+import net.runelite.api.EnumComposition;
+import net.runelite.api.EnumID;
 import net.runelite.api.Varbits;
-import net.runelite.client.game.RunepouchRune;
 
 public class RunePouchState implements ConsumableState {
     private final ItemContainerItem rune1;
     private final ItemContainerItem rune2;
     private final ItemContainerItem rune3;
+    private final ItemContainerItem rune4;
     private final transient String playerName;
 
     public RunePouchState(String playerName, Client client) {
         this.playerName = playerName;
-        rune1 = itemForRune(client.getVarbitValue(Varbits.RUNE_POUCH_RUNE1), client.getVarbitValue(Varbits.RUNE_POUCH_AMOUNT1));
-        rune2 = itemForRune(client.getVarbitValue(Varbits.RUNE_POUCH_RUNE2), client.getVarbitValue(Varbits.RUNE_POUCH_AMOUNT2));
-        rune3 = itemForRune(client.getVarbitValue(Varbits.RUNE_POUCH_RUNE3), client.getVarbitValue(Varbits.RUNE_POUCH_AMOUNT3));
+        final EnumComposition runepouchEnum = client.getEnum(EnumID.RUNEPOUCH_RUNE);
+        rune1 = itemForRune(client.getVarbitValue(Varbits.RUNE_POUCH_RUNE1), client.getVarbitValue(Varbits.RUNE_POUCH_AMOUNT1), runepouchEnum);
+        rune2 = itemForRune(client.getVarbitValue(Varbits.RUNE_POUCH_RUNE2), client.getVarbitValue(Varbits.RUNE_POUCH_AMOUNT2), runepouchEnum);
+        rune3 = itemForRune(client.getVarbitValue(Varbits.RUNE_POUCH_RUNE3), client.getVarbitValue(Varbits.RUNE_POUCH_AMOUNT3), runepouchEnum);
+        rune4 = itemForRune(client.getVarbitValue(Varbits.RUNE_POUCH_RUNE4), client.getVarbitValue(Varbits.RUNE_POUCH_AMOUNT4), runepouchEnum);
     }
 
-    private ItemContainerItem itemForRune(int varbit, int amount) {
-        RunepouchRune rune = RunepouchRune.getRune(varbit);
-        if (rune == null) {
-            return new ItemContainerItem(0, 0);
-        }
-
-        return new ItemContainerItem(rune.getItemId(), amount);
+    private ItemContainerItem itemForRune(int runeId, int amount, EnumComposition runepouchEnum) {
+        return new ItemContainerItem(runepouchEnum.getIntValue(runeId), amount);
     }
 
     @Override
     public Object get() {
-        return new ItemContainerItem[]{rune1, rune2, rune3};
+        return new ItemContainerItem[]{rune1, rune2, rune3, rune4};
     }
 
     @Override
@@ -42,6 +41,6 @@ public class RunePouchState implements ConsumableState {
         if (!(o instanceof RunePouchState)) return false;
         RunePouchState other = (RunePouchState) o;
 
-        return rune1.equals(other.rune1) && rune2.equals(other.rune2) && rune3.equals(other.rune3);
+        return rune1.equals(other.rune1) && rune2.equals(other.rune2) && rune3.equals(other.rune3) && rune4.equals(other.rune4);
     }
 }
